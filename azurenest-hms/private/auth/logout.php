419901fc658bbbbc6d6536d679d863998af3dc2b
yuan123-prog <202311341@fit.edu.ph>
2025-07-02 11:20:59 +0800

    Update logout.php

--- azurenest-hms/private/auth/logout.php
+++ azurenest-hms/private/auth/logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 session_destroy();
-header("Location: /HSM_FINALS_INC/azurenest-hms/public/index.php");
+header("Location: /azurenest-hms/azurenest-hms/azurenest-hms/public/index.php");
 exit();
 ?>
